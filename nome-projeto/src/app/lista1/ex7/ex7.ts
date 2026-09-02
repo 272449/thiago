10b3187b8df1f904dfd357974cedf76171a953bb
272449 <272449@unifio.edu.br>
2026-09-02 01:41:36 +0000

    ex6

--- nome-projeto/src/app/lista1/ex7/ex7.ts
+++ nome-projeto/src/app/lista1/ex7/ex7.ts
@@ -7,3 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './ex7.css',
 })
 export class Ex7 {}
+export class AppComponent {
+  nome = '';}
+  import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';  
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './ex7';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }  
+
