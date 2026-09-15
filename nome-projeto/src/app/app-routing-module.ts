de6a1d4ca0ea58bf1cc73242d1176f8aeb357b37
272449 <272449@unifio.edu.br>
2026-09-15 23:24:06 +0000

    comitnovo

--- nome-projeto/src/app/app-routing-module.ts
+++ nome-projeto/src/app/app-routing-module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
 {
   path: 'lista1',
   loadChildren: () => import('./lista1/lista1-module').then(m => m.Lista1module)
+},
+{
+path: 'lista2',
+  loadChildren: () => import('./lista2/lista2-module').then(m => m.Lista2module)
 }
 ];
 
---
+++ nome-projeto/src/app/lista2/lista2-routing-module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'ex1', component: Ex1 },
+  { path: 'ex2', component: Ex2 },
+  { path: 'ex3', component: Ex3 },
+  { path: 'ex4', component: Ex4 },
+  { path: 'ex5', component: Ex5 },
+  { path: 'ex6', component: Ex6 },
+  { path: 'ex7', component: Ex7 },
+ { path: 'ex8', component: Ex8 },
+  { path: 'ex9', component: Ex9 },
+   { path: 'ex10', component: Ex10 },
+    { path: 'ex11', component: Ex11},
+     { path: 'ex12', component: Ex12 }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class Lista2RoutingModule {}
